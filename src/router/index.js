cd9496fd343123e0c370279e5e54af6d460b64fd
Root <940150534@qq.com>
2018-03-15 13:41:57 +0800

    路由上传

---
+++ src/router/index.js
@@ -0,0 +1,44 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+const Manager = () => import('@/view/manager')
+const Login = () => import('@/view/login')
+const ForgetPsw = () => import('@/view/forgetpsw')
+const ChangePwd = () => import('@/view/changepassword')
+const Register = () => import('@/view/register')
+Vue.use(Router)
+export default new Router({
+  routes: [
+    {
+      path: '/login',
+      component: Login,
+      meta: { userState: 3 }
+    },
+    {
+      path: '/home',
+      component: Manager,
+      meta: { userState: 1 },
+      children: [
+        {
+          path: 'changepwd',
+          component: ChangePwd,
+          meta: { userState: 1 }
+        }
+      ]
+    },
+    {
+      path: '/forgetpsw',
+      component: ForgetPsw
+    },
+    {
+      path: '/register',
+      component: Register
+    },
+    {
+      path: '*',
+      component: Login,
+      meta: { userState: 3 }
+    }
+  ],
+  mode: 'history'
+})
