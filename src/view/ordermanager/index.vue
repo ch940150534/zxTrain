c048778375b2bafa3d3ff0867c3667130c403092
Root <940150534@qq.com>
2018-03-21 16:48:51 +0800

    add ordermanager

---
+++ src/view/ordermanager/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="ordermanager">
+    <package></package>
+    <order-list></order-list>
+  </div>
+</template>
+
+<script>
+  import { EventBus } from '../../util/eventBus'
+  import Package from './package'
+  import Pay from '../../components/pay/index'
+  import OrderList from './orderList'
+
+  export default {
+    components: {
+      OrderList,
+      Pay,
+      Package },
+    name: 'ordermanager',
+    mounted () {
+      EventBus.$emit('setHomeHeader', '账户充值')
+    }
+  }
+</script>
+
+<style>
+</style>
